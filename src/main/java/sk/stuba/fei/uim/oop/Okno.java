b1796a4f7aac82de7b25f992fdb49ef68c9a9240
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-03-29 15:54:25 +0200

    KeyListener a ostatne

--- src/main/java/sk/stuba/fei/uim/oop/Okno.java
+++ src/main/java/sk/stuba/fei/uim/oop/Okno.java
@@ -2,16 +2,16 @@ package sk.stuba.fei.uim.oop;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.*;
 
-public class Okno extends JFrame implements MouseMotionListener {
+public class Okno extends JFrame implements MouseListener, MouseMotionListener, KeyListener {
     public Okno() throws HeadlessException {
         super();
         this.setSize(300, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.addMouseMotionListener(this);
+        this.addMouseListener(this);
+        this.addKeyListener(this);
         this.setVisible(true);
     }
 
@@ -23,5 +23,46 @@ public class Okno extends JFrame implements MouseMotionListener {
     @Override
     public void mouseMoved(MouseEvent e) {
         System.out.println("Move : " + e.getPoint().toString());
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("KLIK : " + e.getPoint().toString());
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("PRESS : " + e.getPoint().toString());
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        System.out.println("RELEASED : " + e.getPoint().toString());
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        System.out.println("EnTERED : " + e.getPoint().toString());
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        System.out.println("EXITED : " + e.getPoint().toString());
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        System.out.println("TYPED " + e.getKeyChar());
+        System.out.println("TYPED " + e.getKeyCode());
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("Presed key " + e.getKeyChar());
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        System.out.println("Released key " + e.getKeyChar());
     }
 }
