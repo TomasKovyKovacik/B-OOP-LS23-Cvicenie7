052760087f45e6cffaa510b51dfe6c325ddf4b09
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-03-29 16:24:17 +0200

    layouty a kalkulacka

--- src/main/java/sk/stuba/fei/uim/oop/Okno.java
+++ src/main/java/sk/stuba/fei/uim/oop/Okno.java
@@ -6,20 +6,46 @@ import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Okno extends JFrame implements ChangeListener {
+public class Okno extends JFrame implements ChangeListener, ActionListener {
+    private int hodnotaSlidera;
+    private JLabel label;
+    private int stlaceneTlacidlo;
+
     public Okno() throws HeadlessException {
         super();
+        this.hodnotaSlidera = 20;
         this.setSize(300, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
 
-        JSlider slider = new JSlider(JSlider.VERTICAL, 0, 50, 22);
+        JPanel menu = new JPanel();
+        menu.setBackground(Color.GREEN);
+        menu.setLayout(new GridLayout(1,2));
+
+        label = new JLabel();
+        this.updateText();
+        menu.add(label);
+
+        JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 50, this.hodnotaSlidera);
         slider.setMinorTickSpacing(1);
         slider.setMajorTickSpacing(10);
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
-        this.add(slider);
+        menu.add(slider);
         slider.addChangeListener(this);
+        slider.setBackground(Color.GREEN);
 
+        this.add(menu, BorderLayout.PAGE_START);
+
+        JPanel klavesnica = new JPanel();
+        klavesnica.setLayout(new GridLayout(3,3));
+
+        for (int i = 1; i < 10; i++) {
+            JButton button = new JButton(String.valueOf(i));
+            klavesnica.add(button);
+            button.addActionListener(this);
+        }
+        this.add(klavesnica, BorderLayout.CENTER);
 
         this.setVisible(true);
     }
@@ -27,5 +53,17 @@ public class Okno extends JFrame implements ChangeListener {
     @Override
     public void stateChanged(ChangeEvent e) {
         System.out.println(((JSlider) e.getSource()).getValue());
+        this.hodnotaSlidera = ((JSlider) e.getSource()).getValue();
+        this.updateText();
+    }
+
+    private void updateText() {
+        label.setText("Hodnota slidera:" + this.hodnotaSlidera + ", Tlacidlo:" + this.stlaceneTlacidlo);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.stlaceneTlacidlo = Integer.parseInt(e.getActionCommand());
+        this.updateText();
     }
 }
