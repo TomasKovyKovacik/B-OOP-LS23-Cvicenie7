a208ab45d6e2035a224a61153ef6f30df741a58a
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-03-29 15:48:53 +0200

    mouseListener

--- src/main/java/sk/stuba/fei/uim/oop/Okno.java
+++ src/main/java/sk/stuba/fei/uim/oop/Okno.java
@@ -2,25 +2,41 @@ package sk.stuba.fei.uim.oop;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class Okno extends JFrame implements ActionListener {
+public class Okno extends JFrame implements MouseListener{
     public Okno() throws HeadlessException {
         super();
         this.setSize(300, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JButton button = new JButton("zavri ma ");
-        this.add(button);
-        button.addActionListener(this);
-
+        this.addMouseListener(this);
         this.setVisible(true);
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        this.dispose();
-        System.exit(0);
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("KLIK : " + e.getPoint().toString());
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("PRESSED : " + e.getPoint().toString());
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        System.out.println("RELEASED : " + e.getPoint().toString());
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        System.out.println("ENTERED : " + e.getPoint().toString());
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        System.out.println("EXITED : " + e.getPoint().toString());
     }
 }
