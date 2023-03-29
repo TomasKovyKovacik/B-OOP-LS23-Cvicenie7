ac7d75825a45447c4b139f216754c42ce7cc65ad
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-03-29 16:02:07 +0200

    key konstanty a adapter

--- src/main/java/sk/stuba/fei/uim/oop/Okno.java
+++ src/main/java/sk/stuba/fei/uim/oop/Okno.java
@@ -4,65 +4,33 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Okno extends JFrame implements MouseListener, MouseMotionListener, KeyListener {
+public class Okno extends UniversalAdapter {
     public Okno() throws HeadlessException {
         super();
         this.setSize(300, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.addMouseMotionListener(this);
-        this.addMouseListener(this);
         this.addKeyListener(this);
         this.setVisible(true);
     }
 
-    @Override
-    public void mouseDragged(MouseEvent e) {
-        System.out.println("DRAG : " + e.getPoint().toString());
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        System.out.println("Move : " + e.getPoint().toString());
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        System.out.println("KLIK : " + e.getPoint().toString());
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        System.out.println("PRESS : " + e.getPoint().toString());
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        System.out.println("RELEASED : " + e.getPoint().toString());
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        System.out.println("EnTERED : " + e.getPoint().toString());
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        System.out.println("EXITED : " + e.getPoint().toString());
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        System.out.println("TYPED " + e.getKeyChar());
-        System.out.println("TYPED " + e.getKeyCode());
-    }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        System.out.println("Presed key " + e.getKeyChar());
+        System.out.println(e);
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W:
+                System.out.println("dopredu");
+                break;
+            case KeyEvent.VK_A:
+                System.out.println("dolava");
+                break;
+            case KeyEvent.VK_D:
+                System.out.println("doprava");
+                break;
+            case KeyEvent.VK_S:
+                System.out.println("dozadu");
+                break;
+        }
     }
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-        System.out.println("Released key " + e.getKeyChar());
-    }
 }
---
+++ src/main/java/sk/stuba/fei/uim/oop/UniversalAdapter.java
@@ -0,0 +1,56 @@
+package sk.stuba.fei.uim.oop;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class UniversalAdapter extends JFrame implements MouseListener, MouseMotionListener, KeyListener {
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
