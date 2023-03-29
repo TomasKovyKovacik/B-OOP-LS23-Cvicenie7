4a681990a30ea37fc9272e9dc2c9b9a1cc6a5a09
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-03-29 16:41:59 +0200

    nefunkcy getCOmponentAt

--- src/main/java/sk/stuba/fei/uim/oop/MojPanel.java
+++ src/main/java/sk/stuba/fei/uim/oop/MojPanel.java
@@ -1,9 +1,14 @@
 package sk.stuba.fei.uim.oop;
 
+import lombok.Setter;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class MojPanel extends JPanel {
+    @Setter
+    private boolean highlight;
+
     public MojPanel() {
         this.setBackground(Color.YELLOW);
     }
@@ -11,6 +16,14 @@ public class MojPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawString("TENTO PREDMET MA NEBAVI", 50,50);
+        g.setColor(Color.BLUE);
+        g.drawRect(3,3,this.getWidth() - 3, this.getHeight()-3);
+        if (this.highlight) {
+            g.setColor(Color.RED);
+            ((Graphics2D) g).setStroke(new BasicStroke(3));
+            g.drawRect(3,3,this.getWidth() - 3, this.getHeight()-3);
+            this.highlight = false;
+        }
+
     }
 }
--- src/main/java/sk/stuba/fei/uim/oop/Okno.java
+++ src/main/java/sk/stuba/fei/uim/oop/Okno.java
@@ -2,15 +2,30 @@ package sk.stuba.fei.uim.oop;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
-public class Okno extends JFrame  {
-
+public class Okno extends UniversalAdapter {
+    private JPanel panel;
     public Okno() throws HeadlessException {
         super();
-        this.setSize(300, 300);
+        this.setSize(700, 700);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.getContentPane().setBackground(Color.ORANGE);
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(2,2));
+        panel.add(new MojPanel());
+        panel.add(new MojPanel());
+        panel.add(new MojPanel());
+        panel.add(new MojPanel());
+        this.add(panel);
+        this.addMouseMotionListener(this);
         this.setVisible(true);
     }
 
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        Component current = this.panel.getComponentAt(e.getPoint());
+        if ((current instanceof MojPanel)) {
+            ((MojPanel) current).setHighlight(true);
+        }
+    }
 }
